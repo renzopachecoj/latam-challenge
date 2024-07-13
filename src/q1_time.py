7b15412fa5c73b44e1b9e73f1f81871d9a3276a4
Renzo <renzopachecoj@gmail.com>
2024-07-13 18:05:13 -0500

    feat: q1, q2, q3 memory and time optimized functions

--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,25 @@
 from typing import List, Tuple
 from datetime import datetime
+import json
+from collections import defaultdict
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    dates = defaultdict(lambda: {'total_tweets': 0, 'users': defaultdict(int)})
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            json_object = json.loads(line)
+            date = datetime.fromisoformat(json_object['date']).date()
+            user = json_object['user']['username']
+            
+            dates[date]['total_tweets'] += 1
+            dates[date]['users'][user] += 1
+    
+    top_10_dates = sorted(dates.items(), key=lambda x: x[1]['total_tweets'], reverse=True)[:10]
+    
+    top_users_for_dates = [
+        (date, max(data['users'].items(), key=lambda x: x[1])[0])
+        for date, data in top_10_dates
+    ]
+    
+    return top_users_for_dates
